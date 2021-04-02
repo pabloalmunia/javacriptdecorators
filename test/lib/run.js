13ce1f8e53fd25f2586a8f678b708b639ba2d062
pabloalmunia <pabloalmunia@gmail.com>
2021-04-02 21:33:32 +0200

    remove previous err file when a new test is runing

--- test/lib/run.js
+++ test/lib/run.js
@@ -4,6 +4,7 @@ const equal     = require ('./equal.js');
 const EXT_ERROR = '.err';
 const readFile  = (file) => fs.readFileSync (path.join (__dirname, file)).toString ();
 const writeFile = (file, data) => fs.writeFileSync (path.join (__dirname, file), data);
+const delFile   = (file) => fs.unlinkSync (path.join (__dirname, file));
 const words     = (source) => source
   .split (/\s|\(|\)|\.|\[|]/)
   .filter (x => x)
@@ -12,12 +13,12 @@ const words     = (source) => source
 module.exports = (label, getTest, getResult, transform, stringify, parse) => {
   
   function readTest (test) {
-    return readFile (`../${ getTest(test) }`);
+    return readFile (`../${ getTest (test) }`);
   }
   
   function readResult (test) {
     try {
-      const sourceResult = readFile (`../${ getResult(test) }`);
+      const sourceResult = readFile (`../${ getResult (test) }`);
       return parse ? parse (sourceResult) : sourceResult;
     } catch (err) {
       return null;
@@ -26,22 +27,31 @@ module.exports = (label, getTest, getResult, transform, stringify, parse) => {
   
   function writeResult (test, source) {
     writeFile (
-      `../${ getResult(test)  }`,
+      `../${ getResult (test) }`,
       stringify ? stringify (source) : source
     );
-    console.log (label, `created ${ getResult(test) }`);
+    console.log (label, `created ${ getResult (test) }`);
   }
   
   function writeError (test, source) {
     writeFile (
-      `../${ getResult( test ) }${ EXT_ERROR }`,
+      `../${ getResult (test) }${ EXT_ERROR }`,
       stringify ? stringify (source) : source
     );
-    console.log (label, `ERROR, created ${ getResult(test) }${ EXT_ERROR }`);
+    console.log (label, `ERROR, created ${ getResult (test) }${ EXT_ERROR }`);
+  }
+  
+  function delError (test) {
+    try {
+      delFile (`../${ getResult (test) }${ EXT_ERROR }`);
+    } catch (err) {
+      void (0);
+    }
   }
   
   return function run (test) {
     console.log ('test', label, test);
+    delError (test);
     const originSource = readTest (test);
     const origin       = transform (originSource);
     const result       = readResult (test);
