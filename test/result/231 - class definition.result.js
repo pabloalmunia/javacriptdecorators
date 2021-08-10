1abfb5a1bdce127cd74460ccfce8d3b16e192430
pabloalmunia <pabloalmunia@gmail.com>
2021-08-10 16:13:11 +0200

    new test for check the hidden class while applying all member decorators

---
+++ test/result/231 - class definition.result.js
@@ -0,0 +1,61 @@
+function getClass(klass) {
+  console.assert(!klass);
+}
+
+try {
+  if (!Symbol.metadata) {
+    Symbol.metadata = Symbol("Symbol.metadata");
+  }
+  const __metadataPrivate = new WeakMap();
+  function __PrepareMetadata(base, kind, property) {
+    const createObjectWithPrototype = (obj, key) => Object.hasOwnProperty.call(obj, key) ? obj[key] : Object.create(obj[key] || Object.prototype);
+    return {
+      getMetadata(key) {
+        if (base[Symbol.metadata] && base[Symbol.metadata][key] && typeof base[Symbol.metadata][key][kind] !== "undefined") {
+          return kind === "public" ? base[Symbol.metadata][key].public[property] : kind === "private" ? __metadataPrivate.has(base[Symbol.metadata][key]) ? __metadataPrivate.get(base[Symbol.metadata][key])[property] : undefined : base[Symbol.metadata][key][kind];
+        }
+      },
+      setMetadata(key, value) {
+        if (typeof key !== "symbol") {
+          throw new TypeError("the key must be a Symbol");
+        }
+        base[Symbol.metadata] = createObjectWithPrototype(base, Symbol.metadata);
+        base[Symbol.metadata][key] = createObjectWithPrototype(base[Symbol.metadata], key);
+        base[Symbol.metadata][key].public = createObjectWithPrototype(base[Symbol.metadata][key], "public");
+        if (!Object.hasOwnProperty.call(base[Symbol.metadata][key], "private")) {
+          Object.defineProperty(base[Symbol.metadata][key], "private", {
+            get() {
+              return Object.values(__metadataPrivate.get(base[Symbol.metadata][key]) || {}).concat(Object.getPrototypeOf(base[Symbol.metadata][key])?.private || []);
+            }
+          });
+        }
+        if (kind === "public") {
+          base[Symbol.metadata][key].public[property] = value;
+        } else if (kind === "private") {
+          if (!__metadataPrivate.has(base[Symbol.metadata][key])) {
+            __metadataPrivate.set(base[Symbol.metadata][key], {});
+          }
+          __metadataPrivate.get(base[Symbol.metadata][key])[property] = value;
+        } else if (kind === "constructor") {
+          base[Symbol.metadata][key].constructor = value;
+        }
+      }
+    };
+  }
+  class __A_ouv4mo {
+    a() {}
+  }
+  __A_ouv4mo.prototype.a = getClass(A)(__A_ouv4mo.prototype.a, {
+    kind: "method",
+    name: "a",
+    isStatic: false,
+    isPrivate: false,
+    ...__PrepareMetadata(__A_ouv4mo.prototype, "public", "a")
+  }) ?? __A_ouv4mo.prototype.a;
+  let A = __A_ouv4mo;
+  Object.defineProperty(A, "name", {
+    value: "A"
+  });
+} catch (e) {
+  console.assert(e.message === "Cannot access 'A' before initialization");
+}
